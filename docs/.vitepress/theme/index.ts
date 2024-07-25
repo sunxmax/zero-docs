25c140b9aed5bdc4aec01937f60cc57ab3925731
sunluyang <your.email@example.com>
2024-07-25 16:18:19 +0800

    添加 vercel analytics

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -3,6 +3,7 @@ import { h } from 'vue'
 import type { Theme } from 'vitepress'
 // import DefaultTheme from 'vitepress/theme'
 import DefaultTheme from 'vitepress/theme-without-fonts'
+import { inject } from "@vercel/analytics"
 import './style.css'
 
 export default {
